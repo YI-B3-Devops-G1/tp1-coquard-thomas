52e630f7d593647d0ed45e36a671f310788dbe44
ThomasYnov-gif <60350521+ThomasYnov-gif@users.noreply.github.com>
2020-01-28 10:56:28 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,8 @@ sudo apt-get install -y openssh-server
 
 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt -y install nodejs
-
 sudo apt-get update
+sudo apt -y install nodejs
 
 sudo apt-get install -y nginx
 sudo systemctl start nginx
